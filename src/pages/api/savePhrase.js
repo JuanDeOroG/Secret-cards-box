391b12a3eeec507185b45db65480d66bb2980713
Juan D. De Oro <juandeoro4@gmail.com>
2023-04-18 13:56:12 -0500

    insertar box, phrases y users

--- src/pages/api/savePhrase.js
+++ src/pages/api/savePhrase.js
@@ -40,31 +40,99 @@ export default function savePhrase(req,res){
                     
                 }
             })
+            //Tarjeta 1
+            const content1 = req.body.input21
+            const title1= req.body.input11
+            //Tarjeta 2
+
+            const content2 = req.body.input22
+            const title2= req.body.input12
+            //Tarjeta 3
+
+            const content3 = req.body.input23
+            const title3= req.body.input13
+            //Tarjeta 4 1
+
+            const content4 = req.body.input24
+            const title4= req.body.input14
+            //Tarjeta 5
+
+            const content5 = req.body.input25
+            const title5= req.body.input15
+            //Tarjeta 6
+
+            const content6 = req.body.input26
+            const title6= req.body.input16
 
             switch (req.body.cantidad) {
                 case "1":
                     
-                    conexion.query(`insert into phrase('content', 'title', box_name) values('')`)
+                    
+                    conexion.query(`insert into phrase(content, title, box_name) values('${content1}','${title1}','${boxname}')`,async (error)=>{
+                        if(error){
+                            console.log(error)
+                        }
+                    })
  
                     break;
                 case "2":
                     
+                    
+                    conexion.query(`insert into phrase(content, title, box_name) values('${content1}','${title1}','${boxname}')`,async (error)=>{
+                        if(error){console.log(error)}else{console.log("Insertado correctamente")}
+                    })
+                    conexion.query(`insert into phrase(content, title, box_name) values('${content2}','${title2}','${boxname}')`,async (error)=>{
+                        if(error){console.log(error)}else{console.log("Insertado correctamente")}
+                    })
+                    
+                    
                     break;
                 case "3":
+                    
+                    
+                    conexion.query(`insert into phrase(content, title, box_name) values('${content1}','${title1}','${boxname}');insert into phrase(content, title, box_name) values('${content2}','${title2}','${boxname}';insert into phrase(content, title, box_name) values('${content3}','${title3}','${boxname}';`,async (error)=>{
+                        if(error){
+                            console.log(error)
+                        }
+                    })
                     
                     break;
                 case "4":
                     
+                    
+                    conexion.query(`insert into phrase(content, title, box_name) values('${content1}','${title1}','${boxname}');insert into phrase(content, title, box_name) values('${content2}','${title2}','${boxname}';insert into phrase(content, title, box_name) values('${content3}','${title3}','${boxname}';insert into phrase(content, title, box_name) values('${content4}','${title4}','${boxname}';`,async (error)=>{
+                        if(error){
+                            console.log(error)
+                        }
+                    })
+                    
                     break;
                 case "5":
+                    
+                    
+                    conexion.query(`insert into phrase(content, title, box_name) values('${content1}','${title1}','${boxname}');insert into phrase(content, title, box_name) values('${content2}','${title2}','${boxname}';insert into phrase(content, title, box_name) values('${content3}','${title3}','${boxname}';insert into phrase(content, title, box_name) values('${content4}','${title4}','${boxname}';insert into phrase(content, title, box_name) values('${content5}','${title5}','${boxname}';`,async (error)=>{
+                        if(error){
+                            console.log(error)
+                        }
+                    })
                     
                     break;
                 case "6":
                     
+                    
+                    conexion.query(`insert into phrase(content, title, box_name) values('${content1}','${title1}','${boxname}');insert into phrase(content, title, box_name) values('${content2}','${title2}','${boxname}';insert into phrase(content, title, box_name) values('${content3}','${title3}','${boxname}';insert into phrase(content, title, box_name) values('${content4}','${title4}','${boxname}';insert into phrase(content, title, box_name) values('${content5}','${title5}','${boxname}';insert into phrase(content, title, box_name) values('${content6}','${title6}','${boxname}';`,async (error)=>{
+                        if(error){
+                            console.log(error)
+                        }
+                    })
+                    
                     break;
                 default:
+                    console.log("No se pudo mandar nada de las tarjetas a la base de datos")
                     break;
             }
+            res.send("La caja y las tarjetas fueron guardadas correctamente. <a href='/boxview?'>Click aqui para ver la caja creada</a>")
+
 
 
 
@@ -72,12 +140,14 @@ export default function savePhrase(req,res){
 
 
         }else{
-            res.send("Tienen que ser maximo 6 tarjetas.")
+            res.send("Tienen que ser maximo 6 tarjetas. Cantidad de tarjetas digitadas: "+req.body.cantidad)
         }
+
+        
 
        
 
-        res.send(req.body.cantidad)
+        
 
     }
 
@@ -85,3 +155,5 @@ export default function savePhrase(req,res){
     
     
 }
+
+//consulta para buscar la caja de un usuario SELECT users.name, boxname, title, content FROM box,users,phrase WHERE users.name = box.username and box.boxname = phrase.box_name
